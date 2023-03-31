fbf0ed799f0effd6fa614f8df8402665c22d029d
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-01 00:05:08 +0330

    feat: add ValidationError

---
+++ def_result/ResultDetails/Errors/ValidationError.py
@@ -0,0 +1,15 @@
+from typing import Any, Dict, List, Optional
+
+from def_result.ResultDetails.ErrorDetail import ErrorDetail
+
+
+class ValidationError(ErrorDetail):
+    """ Represents a validation error."""
+    def __init__(self, title: Optional[str] = "One or more validation errors occurred",
+                 message: Optional[str] = None,
+                 code: Optional[int] = 400,
+                 errors: Optional[Dict[str, str]] = None,
+                 exception: Optional[Exception] = None,
+                 more_data: Optional[List[Any]] = None):
+        super().__init__(title=title, message=message, code=code, errors=errors, exception=exception,
+                         more_data=more_data)
