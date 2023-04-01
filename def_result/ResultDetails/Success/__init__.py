6e2ec3e25ac9d193699702f08ca7cffbcf2dddf2
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-01 23:54:40 +0330

    feat: import files in __init__ files

---
+++ def_result/ResultDetails/Success/__init__.py
@@ -0,0 +1,3 @@
+from def_result.ResultDetails.Success.CreatedDetail import *
+from def_result.ResultDetails.Success.NotModifiedDetail import *
+from def_result.ResultDetails.Success.PartialContentDetail import *
---
+++ def_result/ResultDetails/__init__.py
@@ -0,0 +1,2 @@
+from def_result.ResultDetails.ErrorDetail import *
+from def_result.ResultDetails.SuccessDetail import *
